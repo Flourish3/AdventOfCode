e2a0257f2fe6ac7062b3150f17b60a7b4d65fc05
Johan Tillmar <johan.tillmar@gmail.com>
2019-12-15 21:44:18 +0100

    Some progress on day 14

---
+++ 2019/python/day14.py
@@ -0,0 +1,56 @@
+# Advent of Code - Day 14
+
+import re
+import math
+
+lines = [l.strip() for l in open("../data/input14.txt").readlines()]
+
+groupFind = re.compile(r"((\d+) (\w+))")
+
+
+class Material():
+
+    def __init__(self, name, quantity, ingredients):
+        self.name = name
+        self.created = int(quantity)
+        self.ingrediends = list(map(lambda ingredient: (
+            int(ingredient[0]), ingredient[1]), ingredients))
+        print(self.ingrediends)
+
+    def __repr__(self):
+        return "Name: {}, creates {} from {}".format(self.name, self.created, ", ".join(list(map(lambda x: x[1], self.ingrediends))))
+
+
+materialList = []
+for l in lines:
+    split = l.split("=>")
+    result = re.findall(groupFind, split[1])[0]
+    ingredients = [p[1:] for p in re.findall(groupFind, split[0])]
+
+    materialList.append(Material(result[2], result[1], ingredients))
+
+
+matDict = {m.name: m for m in materialList}
+
+
+def getIngredForMaterial(name, needed, leftOvers):
+    mat = matDict[name]
+    iterationsNeeded = math.ceil(needed/mat.created)
+
+    ingredientsNeeded = list(map(lambda ingredient: (ingredient[0] * iterationsNeeded, ingredient[1]), mat.ingrediends))
+
+    oresNeededList = []
+    for i in ingredientsNeeded:
+
+        if i[1] == "ORE":
+            leftOvers.concat(["ORE"]*(i[0]-needed))
+            oresNeededList.concat(["ORE"]*needed)
+        else:
+            oresNeededList.append(getIngredForMaterial(i[1], i[0], leftOvers))
+
+
+    return oresNeededList
+
+
+leftOvers = []
+print("Part 1: {} ".format(getIngredForMaterial("FUEL", 1, leftOvers)))
